d073ab8a72e731fbe350fcaa5cfa03bb299d18c6
Sollace <sollacea@gmail.com>
2025-03-24 13:23:12 +0000

    Added support for specifying blockmaps on a per-entity basis. Closes #359

--- src/main/java/eu/ha3/presencefootsteps/util/ResourceUtils.java
+++ src/main/java/eu/ha3/presencefootsteps/util/ResourceUtils.java
@@ -2,16 +2,21 @@ package eu.ha3.presencefootsteps.util;
 
 import java.io.InputStreamReader;
 import java.io.Reader;
+import java.util.Map;
 import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.stream.Stream;
+
+import org.jetbrains.annotations.Nullable;
 
 import com.google.gson.JsonObject;
 import com.google.gson.internal.Streams;
 import com.google.gson.stream.JsonReader;
 
 import eu.ha3.presencefootsteps.PresenceFootsteps;
+import net.minecraft.resource.Resource;
+import net.minecraft.resource.ResourceFinder;
 import net.minecraft.resource.ResourceManager;
 import net.minecraft.util.Identifier;
 
@@ -29,7 +34,11 @@ public interface ResourceUtils {
     }
 
     static <T> Stream<T> load(Identifier id, ResourceManager manager, Function<JsonObject, T> reader) {
-        return manager.getAllResources(id).stream().map(res -> {
+        return load(id, manager.getAllResources(id).stream(), reader);
+    }
+
+    static <T> Stream<T> load(Identifier id, Stream<Resource> resources, Function<JsonObject, T> reader) {
+        return resources.map(res -> {
             try (JsonReader stream = new JsonReader(new InputStreamReader(res.getInputStream()))) {
                 return reader.apply(Streams.parse(stream).getAsJsonObject());
             } catch (Exception e) {
@@ -37,5 +46,23 @@ public interface ResourceUtils {
                 return (T)null;
             }
         }).filter(Objects::nonNull);
+    }
+
+    @SuppressWarnings("unchecked")
+    static <T, K, V> Map<K, V> loadDir(ResourceFinder finder, ResourceManager manager,
+            Function<JsonObject, T> reader,
+            Function<Identifier, @Nullable K> keyMapper,
+            Function<Stream<T>, @Nullable V> valueMapper) {
+        return Map.ofEntries(finder.findAllResources(manager).entrySet().stream()
+                .map(entry -> {
+                    K k = keyMapper.apply(entry.getKey());
+                    if (k == null) {
+                        return null;
+                    }
+                    V v = valueMapper.apply(load(entry.getKey(), entry.getValue().stream(), reader));
+                    return v == null ? null : Map.entry(k, v);
+                })
+                .filter(Objects::nonNull)
+                .toArray(Map.Entry[]::new));
     }
 }
